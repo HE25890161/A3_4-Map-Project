91ccfe7c2bd91a7beb8acf44ea0f093f0ece7c8c
HE25890161 <25890161@stu.mmu.ac.uk>
2026-03-22 16:24:54 +0000

    Create home.js script for home page

---
+++ scripts/home.js
@@ -0,0 +1,98 @@
+window.onload = () => {
+    const indexMapLookup = [
+        "Dalton Building",
+        "Geoffrey Manton Building",
+        "Brooks Building"
+    ];
+    const floorLookup = [
+        ["DB-00", "DB-01", "DB-02", "DB-03", "DB-04", "DB-05", "DB-06", "DB-07"],
+        ["MAN-B3", "MAN-B1", "MAN-00", "MAN-01", "MAN-02", "MAN-03", "MAN-04"],
+        ["BR-00", "BR-01", "BR-02", "BR-03", "BR-04"]
+    ];
+
+    let mapSelectOpen = false;
+    let floorSelectOpen = false;
+
+    let map;
+    let mapnumber;
+    let floor;
+    let ready = false;
+
+    document.getElementById("logo").addEventListener("click", () =>{
+        window.location.replace("home.html");
+    });
+
+    document.getElementById("map-box").addEventListener("click", () =>{
+        if (mapSelectOpen){
+            mapSelectOpen = false;
+            document.getElementById("map-select").style.visibility = "collapse";
+        }
+        else{
+            mapSelectOpen = true;
+            document.getElementById("map-select").style.visibility = "visible";
+        }
+    });
+    const mapbuttons = document.getElementsByClassName("map");
+    Array.from(mapbuttons).forEach(mapbutton => {
+        mapbutton.addEventListener("click", () =>{
+            map = mapbutton.textContent;
+            document.getElementById("map-select-label").textContent = "Selected: " + map;
+            document.getElementById("floor-select-label").textContent = "";
+            floor = null;
+            ready = false;
+            document.getElementById("destination-label").textContent = "";
+            floorSelectOpen = false;
+            document.getElementById("floor-select").style.visibility = "collapse";
+            const but = document.getElementById("go-box").style.visibility = "hidden";
+        });
+    }); 
+
+    document.getElementById("floor-box").addEventListener("click", () =>{
+        if (floorSelectOpen){
+            floorSelectOpen = false;
+            document.getElementById("floor-select").style.visibility = "collapse";
+        }
+        else{
+            floorSelectOpen = true;
+            const floorSelect = document.getElementById("floor-select");
+            floorSelect.innerHTML = "";
+            const floortab = document.createElement("tr");
+            const floorheader = document.createElement("th");
+            floorheader.textContent = "Floors";
+            floortab.appendChild(floorheader);
+            let index = indexMapLookup.indexOf(map);
+            if (index == -1){
+                document.getElementById("floor-select-label").textContent = "Select a map first!"
+            }
+            else{
+                floorLookup[index].forEach(floorName => {
+                    const floorelement = document.createElement("td");
+                    floorelement.className = "floor";
+                    floorelement.textContent = floorName;
+                    floortab.appendChild(floorelement);
+                });
+                floorSelect.appendChild(floortab);
+
+                const floorbuttons = document.getElementsByClassName("floor");
+                Array.from(floorbuttons).forEach(floorbutton => {
+                    floorbutton.addEventListener("click", () =>{
+                        floor = floorbutton.textContent;
+                        document.getElementById("floor-select-label").textContent = "Selected: " + floor;
+                        if (map != null && floor !=null){
+                            document.getElementById("destination-label").textContent = map + " - " + floor;
+                            ready = true;
+                            document.getElementById("go-box").style.visibility = "visible";
+                        }
+                    });
+                });
+            }
+
+            floorSelect.style.visibility = "visible";
+        }
+    }); 
+
+
+    document.getElementById("go-button").addEventListener("click", () => {
+        window.location.href = "view.html?building=" + map + "&floor=" + floor;
+    });
+}
